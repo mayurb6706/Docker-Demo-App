545beb44ddfeca183e7076fe09ef494a789bf6a4
Bhosale <mayur.b.bhosale@capgemini.com>
2024-07-04 12:13:46 +0530

    created simple welcome message

--- src/main/java/com/cwm/docker/DockerDemoApplication.java
+++ src/main/java/com/cwm/docker/DockerDemoApplication.java
@@ -2,12 +2,21 @@ package com.cwm.docker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DockerDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DockerDemoApplication.class, args);
 	}
-
+	
+	@GetMapping("/welcome/{name}")
+	public String welcomeMessage(@PathVariable String name) {
+		
+		return "Welcome to docker demo app "+name;
+	}
 }
